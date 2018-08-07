43f6adbaeb2c037102ddc9360d02fc198a37e98f
Sergey Aldoukhov <saldoukhov@gmail.com>
2018-08-07 11:12:40 -0700

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import {createStore} from 'redux';
+import {enthusiasm} from './reducers';
+import {StoreState} from './types';
+
+import Hello from './containers/Hello';
+import {Provider} from 'react-redux';
+
+// @ts-ignore
+const store = createStore<StoreState>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
+});
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <Hello/>
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
