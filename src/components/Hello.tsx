8e50e2cf66622db9d66cc797791146cad698ff0f
Sergey Aldoukhov <saldoukhov@gmail.com>
2018-08-07 12:07:15 -0700

    victory

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './Hello.css';
+import {VictoryPie, VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from "victory";
 
 export interface Props {
     name: string;
@@ -8,20 +9,95 @@ export interface Props {
     onDecrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+const data = [
+    {quarter: 1, earnings: 13000},
+    {quarter: 2, earnings: 16500},
+    {quarter: 3, earnings: 14250},
+    {quarter: 4, earnings: 19000}
+];
+
+const data2012 = [
+    {quarter: 1, earnings: 13000},
+    {quarter: 2, earnings: 16500},
+    {quarter: 3, earnings: 14250},
+    {quarter: 4, earnings: 19000}
+];
+
+const data2013 = [
+    {quarter: 1, earnings: 15000},
+    {quarter: 2, earnings: 12500},
+    {quarter: 3, earnings: 19500},
+    {quarter: 4, earnings: 13000}
+];
+
+const data2014 = [
+    {quarter: 1, earnings: 11500},
+    {quarter: 2, earnings: 13250},
+    {quarter: 3, earnings: 20000},
+    {quarter: 4, earnings: 15500}
+];
+
+const data2015 = [
+    {quarter: 1, earnings: 18000},
+    {quarter: 2, earnings: 13250},
+    {quarter: 3, earnings: 15000},
+    {quarter: 4, earnings: 12000}
+];
+
+
+function Hello({name, enthusiasmLevel = 1, onIncrement, onDecrement}: Props) {
     if (enthusiasmLevel <= 0) {
         throw new Error('You could be a little more enthusiastic. :D');
     }
 
     return (
         <div className="hello">
-            <div className="greeting">
-                Hello {name + getExclamationMarks(enthusiasmLevel)}
-            </div>
-            <div>
-                <button onClick={onDecrement}>-</button>
-                <button onClick={onIncrement}>+</button>
-            </div>
+            {/*<div className="greeting">*/}
+            {/*Hello {name + getExclamationMarks(enthusiasmLevel)}*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*<button onClick={onDecrement}>-</button>*/}
+            {/*<button onClick={onIncrement}>+</button>*/}
+            {/*</div>*/}
+            {/*<VictoryPie/>*/}
+            <VictoryChart
+                theme={VictoryTheme.material}
+                domainPadding={20}
+            >
+                <VictoryAxis
+                    // tickValues specifies both the number of ticks and where
+                    // they are placed on the axis
+                    tickValues={[1, 2, 3, 4]}
+                    tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    // tickFormat specifies how ticks should be displayed
+                    tickFormat={(x) => (`$${x / 1000}k`)}
+                />
+                <VictoryStack colorScale="warm">
+                    <VictoryBar
+                        data={data2012}
+                        x="quarter"
+                        y="earnings"
+                    />
+                    <VictoryBar
+                        data={data2013}
+                        x="quarter"
+                        y="earnings"
+                    />
+                    <VictoryBar
+                        data={data2014}
+                        x="quarter"
+                        y="earnings"
+                    />
+                    <VictoryBar
+                        data={data2015}
+                        x="quarter"
+                        y="earnings"
+                    />
+                </VictoryStack>
+            </VictoryChart>
         </div>
     );
 }
@@ -30,6 +106,6 @@ export default Hello;
 
 // helpers
 
-function getExclamationMarks(numChars: number) {
-    return Array(numChars + 1).join('!');
-}
+// function getExclamationMarks(numChars: number) {
+//     return Array(numChars + 1).join('!');
+// }
